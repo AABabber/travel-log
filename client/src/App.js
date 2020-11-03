571205e04787b7175a9ae9e93fead2ff6d15ffda
Aaron Babber <aaron.a.babber@gmail.com>
2020-11-02 22:46:53 -0600

    Adding markers for existing entries

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,91 @@
 import * as React from 'react';
-import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import { useState, useEffect } from 'react';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
+import { listLogEntries } from './API'
+
+// TODO: Add unique key to Popup components
+// TODO: Update Denver Aquarium coordinates
 const App = () => {
+  const [logEntries, setLogEntries] = useState([]);
+  const [showPopup, setShowPopup] = useState({});
   const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8
+    width: '100vw',
+    height: '100vh',
+    latitude: 37.6,
+    longitude: -95.7,
+    zoom: 3
   });
+
+  useEffect(() => {
+    (async () => {
+      const logEntries = await listLogEntries();
+      setLogEntries(logEntries);
+    })();
+  }, []);
 
   return (
     <ReactMapGL
       {...viewport}
+      mapStyle="mapbox://styles/aababber/ckh1bo9w80ppg19nmd77wylh7"
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       onViewportChange={nextViewport => setViewport(nextViewport)}
-    />
+    >
+      {
+        logEntries.map(entry => (
+          <>
+            <Marker
+              key={entry._id}
+              latitude={entry.latitude}
+              longitude={entry.longitude}
+              offsetLeft={-12}
+              offsetTop={-24}
+            >
+              <div
+                onClick={() => setShowPopup({
+                  // ...showPopup,
+                  [entry._id]: true
+                })}
+              >
+                <svg
+                  className="marker"
+                  style={{
+                    width: `24px`,
+                    height: `24px`
+                  }}
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+                  <circle cx="12" cy="10" r="3"></circle>
+                </svg>
+              </div>
+            </Marker>
+            {
+              showPopup[entry._id] ? (
+                <Popup
+                  latitude={entry.latitude}
+                  longitude={entry.longitude}
+                  closeButton={true}
+                  closeOnClick={false}
+                  onClose={() => setShowPopup({})}
+                  anchor="top" >
+                  <div className="popup">
+                    <h3>{entry.title}</h3>
+                    <p>{entry.comments}</p>
+                    <small>Visited On: {new Date(entry.visitDate).toLocaleDateString()}</small>
+                  </div>
+                </Popup>
+              ) : null
+            }
+          </>
+        ))
+      }
+    </ReactMapGL>
   );
 }
 
